ffc41d3038e517c03720425792a27d53cebb0d50
Antonio Acosta <136067442+AntonioAcostaa@users.noreply.github.com>
2024-01-31 12:25:22 +0100

    Changes components to modules

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { MapApplication } from "./components/application/mapApplication";
+import { MapApplication } from "./modules/application/mapApplication";
 import "@intility/bifrost-react/dist/bifrost-app.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
